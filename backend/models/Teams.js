3bc7b4b374445e2068321dc76456a71a4a53eb02
FaiyazIfaz <103016768+FaiyazIfaz@users.noreply.github.com>
2022-07-03 12:54:46 +0600

    Update Teams.js

--- backend/models/Teams.js
+++ backend/models/Teams.js
@@ -81,15 +81,7 @@ module.exports = {
 
     addTeam: async (team) => {
 
-        // con.connect(async function (err) {
-        //     if (err) throw err;
-        //     console.log("Connected!");
-        //     var sql = `INSERT INTO teams (name, capacity, description) VALUES ('${team.name}', '${team.capacity}', '${team.description}')`;
-        //     con.query(sql, async function (err, result) {
-        //       if (err) throw err;
-        //       team.id = await result.insertId;
-        //     });
-        //   });
+        
         
         return (async () => {
             var sql = `INSERT INTO teams (name, capacity, description, creator) VALUES ('${team.name}', '${team.capacity}', '${team.description}', '${team.creator}')`;
