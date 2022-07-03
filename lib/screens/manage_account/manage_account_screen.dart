f788b1c91173544f0b5d69ef6ebd41992edf3ad2
FaiyazIfaz <103016768+FaiyazIfaz@users.noreply.github.com>
2022-07-03 12:50:04 +0600

    Update manage_account_screen.dart

--- lib/screens/manage_account/manage_account_screen.dart
+++ lib/screens/manage_account/manage_account_screen.dart
@@ -1,19 +1,1 @@
-import 'package:flutter/material.dart';
 
-import 'widgets/body.dart';
-
-class ManageAccountScreen extends StatelessWidget {
-  static Route route() =>
-      MaterialPageRoute(builder: (context) => ManageAccountScreen());
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () => Future.value(false),
-      child: SafeArea(
-        child: Scaffold(
-          body: Body(),
-        ),
-      ),
-    );
-  }
-}
